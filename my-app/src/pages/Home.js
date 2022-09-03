cc89d6ef7928f3754e734ab2cd7e08a4ab325be4
Christa <christanewing47@gmail.com>
2022-09-03 08:57:17 -0400

    updates

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,6 +2,8 @@ const Home = (props) => {
   return (
     <div>
       <h1>This is my Home page</h1>
+      <h2>Click here to view my work → </h2>
+      <button>Projects</button>
     </div>
   );
 };
