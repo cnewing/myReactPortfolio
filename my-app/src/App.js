106ea5817bec86b57f83c5e43671dab7541bace0
Christa <christanewing47@gmail.com>
2022-09-03 09:23:32 -0400

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,26 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import { Route } from "express";
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about-me" component={Services} />
-        <Route path="/projects" component={Products} />
-        <Route path="/contact" component={ContactUs} />
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Route>
+          <Routes>
+            <Route path="/" exact component={<Home />} />
+            <Route path="/about-me" component={<About />} />
+            <Route path="/projects" component={<Projects />} />
+            <Route path="/contact" component={<Contact />} />
+          </Routes>
+        </Route>
+      </Router>
+    </div>
   );
 }
 
