a68af162363ac38682089322d47ca5c1fb834c74
Christa <christanewing47@gmail.com>
2022-09-03 21:07:16 -0400

    updates

---
+++ my-app/src/components/intro/intro.jsx
@@ -0,0 +1,12 @@
+import "./intro.css"
+
+const Intro = () => {
+  return (
+      <div className="i">
+          <div className="i-left">Left IIntro section</div>
+          <div className="i-right">Right Intro section</div>
+      </div>
+  )
+}
+
+export :default Intro;
