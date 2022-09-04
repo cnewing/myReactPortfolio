edd84bca735c0c31a709050684b301674fb97109
Christa <christanewing47@gmail.com>
2022-09-03 22:05:01 -0400

    u

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,13 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/intro";
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 };
-
-// import Container from "react-bootstrap/Container";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-
-// import About from "./pages/About";
-// import Projects from "./pages/Projects";
-// import Contact from "./pages/Contact";
-// import { Route } from "express";
-
-// function App() {
-//   return (
-//     <div>
-//       <Router>
-//         <Navbar />
-//         <Route>
-//           <Routes>
-//             <Route path="/" exact component={<Home />} />
-//             <Route path="/about-me" component={<About />} />
-//             <Route path="/projects" component={<Projects />} />
-//             <Route path="/contact" component={<Contact />} />
-//           </Routes>
-//         </Route>
-//       </Router>
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ my-app/src/components/about/About.jsx
@@ -0,0 +1,6 @@
+import "./about.css";
+const About = () => {
+  return <div className="about-me">About Me</div>;
+};
+
+export default About;
