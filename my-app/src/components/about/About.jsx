636150cc637e7d31e2cff7a24fdf5761c5d3805b
Christa <christanewing47@gmail.com>
2022-09-03 22:21:00 -0400

    add about section

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -1,6 +1,26 @@
 import "./about.css";
+
 const About = () => {
-  return <div className="about-me">About Me</div>;
+  return (
+    <div className="about-me">
+      <div className="a-left">
+        <div className="a-card-bg">
+          <div className="a-img">
+            {/* P L A C E  A N O T H E R  I M A G E  H E R E */}
+          </div>
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-subtitle">About Me</h1>
+        <p className="a-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          dolorem quidem non voluptatum vero itaque blanditiis, consectetur
+          voluptate eius dignissimos architecto aliquam reprehenderit a
+          repudiandae deserunt id culpa recusandae porro!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
