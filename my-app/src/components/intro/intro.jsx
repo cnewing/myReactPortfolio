55768792736c086c0dd862f43d4b07e9b3149723
Christa <christanewing47@gmail.com>
2022-09-03 21:31:56 -0400

    setting up introduction section

--- my-app/src/components/intro/intro.jsx
+++ my-app/src/components/intro/intro.jsx
@@ -1,12 +1,24 @@
-import "./intro.css"
+import "./intro.css";
 
 const Intro = () => {
   return (
-      <div className="i">
-          <div className="i-left">Left IIntro section</div>
-          <div className="i-right">Right Intro section</div>
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-greet"> Hey, my name is </h2>
+          <h1 className="i-name"> Christa Newing</h1>
+          <div className="i-roles">
+            <div className="i-roles-wrapper">
+              <div className="i-role1">Web Developer</div>
+              <div className="i-role1">UI Designer</div>
+              <div className="i-role1">Graphic Designer</div>
+            </div>
+          </div>
+        </div>
       </div>
-  )
-}
+      <div className="i-right">Right Intro section</div>
+    </div>
+  );
+};
 
-export :default Intro;
+export default Intro;
