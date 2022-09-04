2305352368899712d29473c9eb4f6bbef3b9616d
Christa <christanewing47@gmail.com>
2022-09-03 21:49:35 -0400

    more styling

--- my-app/src/components/intro/intro.jsx
+++ my-app/src/components/intro/intro.jsx
@@ -1,4 +1,5 @@
 import "./intro.css";
+import Photo from "../../images/placeholder.jpeg";
 
 const Intro = () => {
   return (
@@ -14,9 +15,15 @@ const Intro = () => {
               <div className="i-role1">Graphic Designer</div>
             </div>
           </div>
+          <p className="i-about">
+            I love making design ideas become design realities.
+          </p>
         </div>
       </div>
-      <div className="i-right">Right Intro section</div>
+      <div className="i-right">
+        <div className="image-bg"></div>
+        <img src="" alt="" className="my-pic" />
+      </div>
     </div>
   );
 };
